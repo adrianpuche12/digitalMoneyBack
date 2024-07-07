2fce25cf4510df2f4c6c63bb398db612e6858f27
Pialeonor12 <jorgeadrian_pucheta@hotmail.com>
2024-07-06 21:33:19 -0300

    cambios

---
+++ src/main/java/digitalMoney/digitalMoneyDH/DigitalMoneyDhApplication.java
@@ -0,0 +1,13 @@
+package digitalMoney.digitalMoneyDH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMoneyDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMoneyDhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalMoney/digitalMoneyDH/DigitalMoneyDhApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalMoney.digitalMoneyDH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMoneyDhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
